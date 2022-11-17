3f831b3690fb52f0c815f194ff025577b07fd34d
CrimsonR4ven <qisiel@protonmail.com>
2022-11-17 22:46:59 +0100

    Revert "funny"
    This reverts commit f158193ca65bc645ed64ef382867fdfc8b06cfad.

---
+++ ALXCourseHomework/MailingService/HomeworkTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace ALXCourseHomework.MailingService
+{
+    public class HomeworkTest1
+    {
+        public static void Run()
+        {
+            var emailUno = new Email("partigiano@protonmail.com", "dittatore@gmail.com", "Una mattina mi sono svegliato", "e ho trovato l'invasor.");
+            var emailDue = new Email("partigiano@protonmail.com", "dittatore@gmail.com", "O partigiano, portami via", "ché mi sento di morir.");
+            var emailTre = new Email("partigiano@protonmail.com", "dittatore@gmail.com", "E se io muoio da partigiano", "tu mi devi seppellir.");
+            var emailQuattro = new Email("partigiano@protonmail.com", "dittatore@gmail.com", "E seppellire lassù in montagna", "sotto l'ombra di un bel fior.");
+            var emailCinque = new Email("partigiano@protonmail.com", "dittatore@gmail.com", "Tutte le genti che passeranno", "Mi diranno Che bel fior!");
+
+            EmailService.SendEmail(emailUno);
+            EmailService.SendEmail(emailDue);
+            EmailService.SendEmail(emailTre);
+            EmailService.SendEmail(emailQuattro);
+            EmailService.SendEmail(emailCinque);
+        }
+    }
+}
---
+++ AlxCourseHomework/MaterialAssignments/VariablesHomework.cs
@@ -0,0 +1,13 @@
+﻿namespace AlxCourseHomework.MaterialAssignments
+{
+    public class VariablesHomework
+    {
+        public static void PersonalData()
+        {
+            string name = "Ronald", surname = "McDonald", gender = "Military grade potato";
+            int age = 9573, height = 13; 
+            long peselNumber = 13687213674;
+            Console.WriteLine($"Name: {name}\nSurname: {surname}\nGender: {gender}\nAge: {age}\nHeight: {height}\nPESEL: {peselNumber}");
+        }
+    }
+}
