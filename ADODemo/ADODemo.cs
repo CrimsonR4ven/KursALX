299b8a0bc289fd9808e605c75841e5f8b1ace46f
CrimsonR4ven <qisiel@protonmail.com>
2023-01-25 19:05:45 +0100

    skibidi dop dop dop dop yes yes yes yes dop skibidi w w w

---
+++ ADODemo/ADODemo.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ADODemo
+{
+    public class ADODemos
+    { 
+        public void Run(string connectionString,string val, string val2)
+        {
+            var queryString = "select @a from Movie m";
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(queryString, conn);
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@a";
+                param.Value = val;
+                param.SqlDbType = SqlDbType.Xml;
+                command.Parameters.Add(param);
+                try
+                {
+                    conn.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read()) { 
+                    Console.WriteLine(reader.GetString(0));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+
+    }
+}
---
+++ ADODemo/Program.cs
@@ -0,0 +1,5 @@
+﻿using ADODemo;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+ADODemos de = new ADODemos();
+de.Run("Data Source=RUBY\\SQLEXPRESS;Initial Catalog=RentMovieOnDrugs;Integrated Security=true;Connect Timeout=30", "[MovieName]","v");
